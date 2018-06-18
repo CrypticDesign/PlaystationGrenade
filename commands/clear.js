de02b8190266b09fe7d585efaf92c35cfec66332
Mintical <mashoobee@gmail.com>
2018-06-17 18:59:04 -0700

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     
     message.delete().catch(O_o => {});
     
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have permission to use this command!").then(msg => msg.delete(5000));
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You do not have permission to use this command!").then(msg => msg.delete(5000));
     if(!args[0]) return message.channel.send("Please specify the number of messages you are deleting!").then(msg => msg.delete(5000));
     message.channel.bulkDelete(args[0]).then(() => {
         message.channel.send(`Cleared ${args[0]} messages. :bomb:`).then(msg => msg.delete(5000));
