9f8594d6b5029e20c1fa3ed3e3319c2c7726c9f6
Mintical <mashoobee@gmail.com>
2018-06-17 18:25:45 -0700

    Add files via upload

---
+++ commands/serverinfo.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+    
+    message.delete().catch(O_o => {});
+    
+    let servericon = message.guild.displayAvatarURL;
+    let serverembed = new Discord.RichEmbed()
+     .setDescription("**Server Information**")
+     .setColor(botconfig.blue)
+     .setThumbnail(servericon)
+     .addField("Server Name", message.guild.name)
+     .addField("Created On", message.guild.createdAt)
+     .addField("You Joined", message.member.joinedAt)
+     .addField("Total Members", message.guild.memberCount);
+     
+     return message.channel.send(serverembed);
+    
+}
