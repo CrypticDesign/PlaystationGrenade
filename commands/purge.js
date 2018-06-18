9a6ecf72e710b738415c705868fd239e965fbd02
Mintical <mashoobee@gmail.com>
2018-06-17 18:39:16 -0700

    Add files via upload

---
+++ commands/purge.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.author.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have permission to use this command!").then(msg.delete(5000));
+    if(!args[0]) return message.channel.send("Please specify the number of messages you want to delete!").then(msg.delete(5000));
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`Cleared ${args[0} messages. :bomb:`).then(msg.delete(5000));
+    });
+}
+
+module.exports.help = {
+    name: "purge"
+}
