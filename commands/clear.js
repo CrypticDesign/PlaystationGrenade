85ec1fbd4f7cfee1e13e12c5039e1279961ff2c4
Mintical <mashoobee@gmail.com>
2018-06-17 18:53:53 -0700

    Add files via upload

---
+++ commands/clear.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    
+    message.delete().catch(O_o => {});
+    
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have permission to use this command!").then(msg => msg.delete(5000));
+    if(!args[0]) return message.channel.send("Please specify the number of messages you are deleting!").then(msg => msg.delete(5000));
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`Cleared ${args[0]} messages. :bomb:`).then(msg => msg.delete(5000));
+    });
+}
+
+module.exports.help = {
+    name: "clear"
+}
