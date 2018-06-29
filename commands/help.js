01db6ebc02a0a3c2b64bca56731d95177471d17b
Mintical <mashoobee@gmail.com>
2018-06-28 22:14:25 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,12 +12,10 @@ module.exports.run = async (bot, message, args) => {
      .setTitle('Commands')
      .setThumbnail(hicon)
      .setColor(botconfig.blue)
-     .addField("help", "Gives you a list of commands!")
-     .addField("ping", "Pong! (Checks Ping)")
-     .addField("weather", "Check the weather in a certain area! [Usage: ps!weather <location>]")
-     .addField("serverinfo", "Check the info of the server you're in!")
-     .addField("clear", "Clear Messages (Moderator Only)");
-     
+     .addField("Support", "**help** - Gives you a list of commands!\n**serverinfo** - Check the info of the server you're in!\n**ping** - Checks Ping!")
+     .addField("Special", "**weather** - Checks the weather in a certain area!")
+     .addField("Moderator", "**clear** - Clear a certain ammount of messages!")
+
     return message.channel.send(hembed);
     
 }
